040b2f08758a08cd9de0b31ba07666ed8f333eb3
Wakjira-Tesama <wakjiratesama@gmail.com>
2026-07-05 14:40:00 +0300

    feat: configure providers and exports in AnomalySensorModule

--- src/modules/anomaly-sensor/anomaly-sensor.module.ts
+++ src/modules/anomaly-sensor/anomaly-sensor.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
+import { BullModule } from '@nestjs/bull';
+import { QUEUE_NAMES } from '../queues/queues.constants';
+import { PrismaModule } from '../../prisma/prisma.module';
+import { AlertingService } from './alerting.service';
+import { AnomalySensorService } from './anomaly-sensor.service';
 
 /**
- * AnomalySensorModule - Initial module scaffold for anomaly detection.
- * This module will house all anomaly detection and alerting services.
+ * AnomalySensorModule - Connects the anomaly detection engine (AnomalySensorService)
+ * and alerting mechanisms (AlertingService) to the rest of the application.
+ * Imports Prisma for audit logging and Bull for alert dispatching.
  */
-@Module({})
+@Module({
+  imports: [
+    PrismaModule,
+    BullModule.registerQueue({
+      name: QUEUE_NAMES.NOTIFICATIONS,
+    }),
+  ],
+  providers: [AlertingService, AnomalySensorService],
+  exports: [AnomalySensorService],
+})
 export class AnomalySensorModule {}
